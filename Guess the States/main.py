96d5ef1d189b9027a9411da3cef47b3db496c80f
Siddharth Joshi <siddharthjoshi329@gmail.com>
2023-07-21 14:28:12 -0400

    Update main.py

--- Guess the States/main.py
+++ Guess the States/main.py
@@ -4,7 +4,7 @@ from country_text import CountryText
 
 
 # Setting up the proper image path.
-IMAGE_PATH = "blank_states_img.gif"
+IMAGE_PATH = "india_map.gif"
 MAX_SCORE = 50
 
 main_screen = turtle.Screen()
@@ -24,34 +24,34 @@ data_frame = pandas.read_csv("50_states.csv")
 game_continue = True
 title = "Guess the State"
 current_score = 0
-correct_guesses = []
+guessed_states = []
 
-while game_continue:
+# Storing the states inside the list.
+us_states = data_frame["state"].to_list()
 
-    # If the user guessed the correct state, then this will store the row of that state.
+while len(guessed_states) < 50:
+
+    # The guessed state will be stored here.
     answer = main_screen.textinput(title=title, prompt="What's another state's name?").title()
-    states_name = data_frame[data_frame["state"] == answer]
-    print(states_name)
-
-    # Converting the Series into a List.
-    state_x_coordinate = states_name["x"].tolist()
-    state_y_coordinate = states_name["y"].tolist()
-
-    """ If the user's guess was not correct, then the generated list will be empty, so we continue the
-    iteration of the loop and keep prompting the user. """
-    if not state_x_coordinate:
-        continue
 
     # If the user's answer was already guessed, then this will let them know.
-    if answer in correct_guesses:
+    if answer in guessed_states:
         main_screen.textinput(title=title, prompt=f"{answer} already Guessed! Click Ok to continue.")
         continue
 
-    """ If the user's guess is valid, then we convert the list to float and instantiate the Turtle
-    Object displaying the text and making it move to a specific location based on the .CSV file."""
-    text = CountryText(float(state_x_coordinate[0]), float(state_y_coordinate[0]), answer)
-    current_score += 1
-    title = f"{current_score} / {MAX_SCORE} States Correct"
-    correct_guesses.append(answer)
+    if answer == "Exit":
+        missing_states = [state for state in us_states if state not in guessed_states]
+        missing_state_frame = pandas.DataFrame(missing_states)
+        missing_state_frame.to_csv('states_to_remember.csv')
+        break
+
+    if answer in us_states:
+        guessed_states.append(answer)
+        state_data = data_frame[data_frame["state"] == answer]
+
+        # Can also use 'states_name["state"].item()' instead of 'answer'
+        text = CountryText(int(state_data["x"]), int(state_data["y"]), answer)
+        current_score += 1
+        title = f"{current_score} / {MAX_SCORE} States Correct"
 
 main_screen.exitonclick()
